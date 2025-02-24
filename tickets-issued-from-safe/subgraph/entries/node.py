c493cbb01593bb9e7eea1826df37dc4d2e6ec4c5
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-02-24 13:21:41 +0100

    link safe to tickets (#16)

--- tickets-issued-from-safe/subgraph/entries/node.py
+++ tickets-issued-from-safe/subgraph/entries/node.py
@@ -2,10 +2,11 @@ from lib.subgraph import Entry
 
 
 class Node(Entry):
-    def __init__(self,id: str):
+    def __init__(self, id: str):
         self.id = id
 
     @classmethod
     def fromSubgraphResult(cls, node: dict):
         return cls(node["node"]['id'])
+
 
