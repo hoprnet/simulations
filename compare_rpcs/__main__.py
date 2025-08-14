6ca133ee5c0391e3d3e55b217587d452a2954b4b
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-08-14 14:40:39 +0200

    loading bar and tracking discrepancies

--- compare_rpcs/__main__.py
+++ compare_rpcs/__main__.py
@@ -4,7 +4,7 @@ import json
 import click
 
 from lib.colors import Color
-from lib.helper import asynchronous
+from lib.helper import asynchronous, progress_bar
 from lib.rpc.entries.log import Log
 from lib.rpc.entries.types.block import Block
 from lib.rpc.query_provider import ETHGetLogsRPCProvider
@@ -32,19 +32,30 @@ async def main(urls: list[RPCUrl], from_block: Block, to_block: Block):
     logs: dict[str, list[dict]] = {}
 
     for item in urls: 
-        print(*Color.BOLD_STRING, sep=item.short_url)
+        print(*Color.BOLD_STRING, sep=item.url)
         logs[item.url] = []
 
         for start, end in get_ranges(from_block, to_block, MAX_SINGLE_QUERY_BLOCK_RANGE):
-            print(*Color.BOLD_STRING, sep=f"Range: {start} - {end}")
+            # print(*Color.BOLD_STRING, sep=f"Range: {start} - {end}")
+            progress_bar(start.idx, to_block.idx, percentage=(start.idx - from_block.idx) / (to_block.idx - from_block.idx))
 
             sub_logs: list[Log] = await ETHGetLogsRPCProvider(item.url).get(
                     fromBlock=start.idx,
                     toBlock=end.idx,
-                    address="0x77C9414043d27fdC98A6A2d73fc77b9b383092a7",
-                topics=["0xdd90f938230335e59dc925c57ecb0e27a28c2d87356e31f00cd5554abd6c1b2d"]
+                topics=[]
             )
-            logs[item.url].extend([log.as_dict for log in sub_logs])            
+            logs[item.url].extend([log.as_dict for log in sub_logs])
+        print(f" -> Found {len(logs[item.url])} logs")
+
+
+    discrepancies_found = False
+    for values in zip(*logs.values()):
+        if not all(value == values[0] for value in values):
+            print(f"Discrepancy found: {values=}")
+            discrepancies_found = True
+
+    if not discrepancies_found:
+        print("No discrepancies found.")
 
     with open("logs.json", "w") as f:
         f.write(json.dumps(logs, indent=4))
